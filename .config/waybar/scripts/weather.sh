492889ee09fa4145fc043c20d36992c0f6811407
Raphael Sá <dev.raphaelsa@gmail.com>
2024-10-26 00:49:41 -0300

    Updating

--- .config/waybar/scripts/weather.sh
+++ .config/waybar/scripts/weather.sh
@@ -1,24 +1,71 @@
-#!/bin/sh
-BSSIDS="$(nmcli device wifi list |
-    awk 'NR>1 {if ($1 != "*") {print $1}}' |
-    tr -d ":" |
-    tr "\n" ",")"
+#!/bin/bash
 
-LOC=""
-REQUEST_GEO="$(wget -qO - http://openwifi.su/api/v1/bssids/"$BSSIDS")"
-if [[ "$(jq ".count_results" <<< "$REQUEST_GEO")" -gt 0 ]] ; then
-    LAT="$(jq ".lat" <<< "$REQUEST_GEO")"
-    LON="$(jq ".lon" <<< "$REQUEST_GEO")"
-    LOC="$LAT,$LON"
+cachedir=~/.cache/waybar
+cachefile=weather
+
+if [ ! -d $cachedir ]; then
+    mkdir -p $cachedir
 fi
 
-text="$(curl -s "https://wttr.in/$LOC?format=1" | sed 's/ //g')"
-tooltip="$(curl -s "https://wttr.in/$LOC?0QT" |
-    sed 's/\\/\\\\/g' |
-    sed ':a;N;$!ba;s/\n/\\n/g' |
-    sed 's/"/\\"/g')"
-
-if ! grep -q "Unknown location" <<< "$text"; then
-    echo "{\"text\": \"$text\", \"tooltip\": \"<tt>$tooltip</tt>\", \"class\": \"weather\"}"
+if [ ! -f $cachedir/$cachefile ]; then
+    touch $cachedir/$cachefile
 fi
 
+SAVEIFS=$IFS
+IFS=$'\n'
+
+cacheage=$(($(date +%s) - $(stat -c '%Y' "$cachedir/$cachefile")))
+if [ $cacheage -gt 1740 ] || [ ! -s $cachedir/$cachefile ]; then
+    data=($(curl -s https://en.wttr.in/$1\?0qnT 2>&1))
+    echo ${data[0]} | cut -f1 -d, > $cachedir/$cachefile
+    echo ${data[1]} | sed -E 's/^.{15}//' >> $cachedir/$cachefile
+    echo ${data[2]} | sed -E 's/^.{15}//' | sed 's/ °.*/°/' >> $cachedir/$cachefile
+fi
+
+weather=($(cat $cachedir/$cachefile))
+
+IFS=$SAVEIFS
+
+temperature=$(echo ${weather[2]} | sed -E 's/([[:digit:]]+)\.\./\1 to /g')
+
+case $(echo ${weather[1]##*,} | tr '[:upper:]' '[:lower:]') in
+"clear" | "sunny")
+    condition="☀"
+    ;;
+"partly cloudy")
+    condition="⛅"
+    ;;
+"cloudy")
+    condition=""
+    ;;
+"overcast")
+    condition="🌦"
+    ;;
+"mist" | "fog" | "freezing fog")
+    condition=""
+    ;;
+"patchy rain possible" | "patchy light drizzle" | "light drizzle" | "patchy light rain" | "light rain" | "light rain shower" | "rain")
+    condition="🌦"
+    ;;
+"moderate rain at times" | "moderate rain" | "heavy rain at times" | "heavy rain" | "moderate or heavy rain shower" | "torrential rain shower" | "rain shower")
+    condition="⛆"
+    ;;
+"patchy snow possible" | "patchy sleet possible" | "patchy freezing drizzle possible" | "freezing drizzle" | "heavy freezing drizzle" | "light freezing rain" | "moderate or heavy freezing rain" | "light sleet" | "ice pellets" | "light sleet showers" | "moderate or heavy sleet showers")
+    condition="❄"
+    ;;
+"blowing snow" | "moderate or heavy sleet" | "patchy light snow" | "light snow" | "light snow showers")
+    condition="⛄"
+    ;;
+"blizzard" | "patchy moderate snow" | "moderate snow" | "patchy heavy snow" | "heavy snow" | "moderate or heavy snow with thunder" | "moderate or heavy snow showers")
+    condition="❄"
+    ;;
+"thundery outbreaks possible" | "patchy light rain with thunder" | "moderate or heavy rain with thunder" | "patchy light snow with thunder")
+    condition="🌩"
+    ;;
+*)
+    condition=""
+    echo -e "{\"text\":\""$condition"\", \"alt\":\""${weather[0]}"\", \"tooltip\":\""${weather[0]}: $temperature ${weather[1]}"\"}"
+    ;;
+esac
+
+echo -e "{\"text\":\""$condition $temperature"\", \"alt\":\""${weather[0]}"\", \"tooltip\":\""${weather[0]}: $temperature ${weather[1]}"\"}"
